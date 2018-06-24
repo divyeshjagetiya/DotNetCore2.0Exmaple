cd3e568d759cfeecd19e68aa7d7d6b8ffc6adb99
Divyesh Jagetiya <djagetiya@XE-T-DIVYESHJAG>
2018-06-24 16:52:40 +0530

    Initial Commit

---
+++ SimpleImageGallery.Data/SimpleImageGalleryDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SimpleImageGallery.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace SimpleImageGallery.Data
+{
+    public class SimpleImageGalleryDbContext : DbContext
+    {
+        public SimpleImageGalleryDbContext(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        public DbSet<GalleryImage> GalleryImages { get; set; }
+        public DbSet<ImageTag> ImageTags { get; set; }
+    }
+}
+
---
+++ SimpleImageGallery.Data/SimpleImageGalleryDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace SimpleImageGallery.Data
+{
+    public class SimpleImageGalleryDbContextFactory : IDesignTimeDbContextFactory<SimpleImageGalleryDbContext>
+    {
+        public SimpleImageGalleryDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<SimpleImageGalleryDbContext>();
+            builder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SimpleImageGallery;Trusted_Connection=True;MultipleActiveResultSets=true",
+                optionsBuilder => optionsBuilder.MigrationsAssembly(typeof(SimpleImageGalleryDbContext).GetTypeInfo().Assembly.GetName().Name));
+            return new SimpleImageGalleryDbContext(builder.Options);
+        }
+    }
+}
