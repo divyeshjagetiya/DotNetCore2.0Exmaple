c5f44a2578bf1f94621dcb0ff151476971609da4
Divyesh Jagetiya <djagetiya@XE-T-DIVYESHJAG>
2018-06-25 15:53:52 +0530

    Azure Blob Container Added

--- SimpleImageGallery.Services/ImageService.cs
+++ SimpleImageGallery.Services/ImageService.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using SimpleImageGallery.Data;
 using SimpleImageGallery.Data.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SimpleImageGallery.Services
 {
@@ -15,6 +18,7 @@ namespace SimpleImageGallery.Services
         {
             _ctx = ctx;
         }
+
         public IEnumerable<GalleryImage> GetAll()
         {
             return _ctx.GalleryImages.Include(img => img.Tags);
@@ -28,6 +32,37 @@ namespace SimpleImageGallery.Services
         public IEnumerable<GalleryImage> GetWithTag(string tag)
         {
            return GetAll().Where(img => img.Tags.Any(t => t.Description == tag));
+        }
+
+        public CloudBlobContainer GetBlobContainer(string azureConnectionString, string containerName)
+        {
+            var storageAccount = CloudStorageAccount.Parse(azureConnectionString);
+            var blobClient = storageAccount.CreateCloudBlobClient();
+            return blobClient.GetContainerReference(containerName);
+        }
+
+        public async Task SetImage(string title, string tags, Uri uri)
+        {
+            var image = new GalleryImage
+            {
+                Title = title,
+                Tags = ParseTags(tags),
+                Url = uri.AbsoluteUri,
+                Created = DateTime.Now
+            };
+            _ctx.Add(image);
+            await _ctx.SaveChangesAsync();
+
+        }
+        public List<ImageTag> ParseTags(string tags)
+        {
+            var tagList = tags.Split(",").ToList();
+            var imageTags = new List<ImageTag>();
+            foreach(var tag in tagList)
+            {
+                imageTags.Add(new ImageTag { Description = tag });
+            }
+            return imageTags;
         }
     }
 }
